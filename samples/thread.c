9a086bc88bfd7d1d658e1b0c3d23b916e0a3298b
Caniro <caniro@naver.com>
2021-02-05 21:37:34 +0900

    20210205 Add messages for pthread_join test

--- samples/thread.c
+++ samples/thread.c
@@ -6,7 +6,7 @@
 /*   By: yuhan <yuhan@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/03 19:29:23 by yuhan             #+#    #+#             */
-/*   Updated: 2021/02/03 20:50:09 by yuhan            ###   ########.fr       */
+/*   Updated: 2021/02/05 21:36:37 by yuhan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,24 @@
 #include <pthread.h>
 #include <stdio.h>
 
-void		*func_thread(void *name)
+void		*func_thread1(void *name)
 {
 	printf("I'm %s\n", name);
+	sleep(3);
+	return (NULL);
+}
+
+void		*func_thread2(void *name)
+{
+	printf("I'm %s\n", name);
+	sleep(2);
+	return (NULL);
+}
+
+void		*func_thread3(void *name)
+{
+	printf("I'm %s\n", name);
+	sleep(1);
 	return (NULL);
 }
 
@@ -39,18 +54,22 @@ int			main(void)
 
 	for (int i = 0; i < 3; ++i)
 	{
-		if (pthread_create(&threads[0], NULL, func_thread, t1))
+		if (pthread_create(&threads[0], NULL, func_thread1, t1))
 			error_msg("Fail to create thread 1\n.");
-		if (pthread_create(&threads[1], NULL, func_thread, t2))
+		if (pthread_create(&threads[1], NULL, func_thread2, t2))
 			error_msg("Fail to create thread 2\n.");
-		if (pthread_create(&threads[2], NULL, func_thread, t3))
+		if (pthread_create(&threads[2], NULL, func_thread3, t3))
 			error_msg("Fail to create thread 3\n.");
 		if (pthread_join(threads[0], &status[0]))
 			error_msg("Fail to create join 1\n.");
+		printf("This message is printed after thread1 join.\n");
 		if (pthread_join(threads[1], &status[1]))
 			error_msg("Fail to create join 2\n.");
+		printf("This message is printed after thread2 join.\n");
 		if (pthread_join(threads[2], &status[2]))
 			error_msg("Fail to create join 3\n.");
-		printf("\n");
+		printf("This message is printed after thread3 join.\n");
+		printf("This message is printed after all threads join.\n");
 	}
+	printf("Thus, pthread_join function waits until its thread ends.\n");
 }
